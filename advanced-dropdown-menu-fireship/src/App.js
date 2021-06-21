632b506b70614982c40505ab9934da1da0936c16
matheus <matheus.xmaz10@gmail.com>
2021-06-20 21:18:43 -0300

    Top navbar

--- advanced-dropdown-menu-fireship/src/App.js
+++ advanced-dropdown-menu-fireship/src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
+import {ReactComponent as BellIcon} from './icons/bell.svg'
+import {ReactComponent as MessengerIcon} from './icons/messenger.svg'
+import {ReactComponent as CaretIcon} from './icons/caret.svg'
+import {ReactComponent as PlusIcon} from './icons/plus.svg'
+import {ReactComponent as CogIcon} from './icons/cog.svg'
+import {ReactComponent as ChevronIcon} from './icons/chevron.svg'
+import {ReactComponent as ArrowIcon} from './icons/arrow.svg'
+import {ReactComponent as BoltIcon} from './icons/bolt.svg'
+
+function Navbar(props) {
+  return(
+  <nav className="navbar">
+    <ul className="navbar-nav">
+      {props.children}
+    </ul>
+  </nav>
+  )
+}
+function NavItem(props){
+  return(
+    <li className="nav-item">
+      <a href="" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  )
+}
 
 function App() {
   return (
+    <Navbar>
+      <NavItem icon={<PlusIcon/>}/>
+      <NavItem icon={<BellIcon/>}/>
+      <NavItem icon={<MessengerIcon/>}/>
+    </Navbar>
   );
 }
 
