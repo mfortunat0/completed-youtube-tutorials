e144877d0746733b67d03e8e1a911e96a51fce20
matheus <matheus.xmaz10@gmail.com>
2021-06-20 22:00:30 -0300

    Dropdown

--- advanced-dropdown-menu-fireship/src/App.js
+++ advanced-dropdown-menu-fireship/src/App.js
@@ -1,28 +1,61 @@
 import './App.css';
-import {ReactComponent as BellIcon} from './icons/bell.svg'
-import {ReactComponent as MessengerIcon} from './icons/messenger.svg'
-import {ReactComponent as CaretIcon} from './icons/caret.svg'
-import {ReactComponent as PlusIcon} from './icons/plus.svg'
-import {ReactComponent as CogIcon} from './icons/cog.svg'
-import {ReactComponent as ChevronIcon} from './icons/chevron.svg'
-import {ReactComponent as ArrowIcon} from './icons/arrow.svg'
-import {ReactComponent as BoltIcon} from './icons/bolt.svg'
+import { useState } from 'react'
+import { ReactComponent as BellIcon } from './icons/bell.svg'
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg'
+import { ReactComponent as CaretIcon } from './icons/caret.svg'
+import { ReactComponent as PlusIcon } from './icons/plus.svg'
+import { ReactComponent as CogIcon } from './icons/cog.svg'
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg'
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg'
+import { ReactComponent as BoltIcon } from './icons/bolt.svg'
+
 
 function Navbar(props) {
-  return(
-  <nav className="navbar">
-    <ul className="navbar-nav">
-      {props.children}
-    </ul>
-  </nav>
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        {props.children}
+      </ul>
+    </nav>
   )
 }
-function NavItem(props){
-  return(
+
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">
+          {props.leftIcon}
+        </span>
+        {props.children}
+        <span className="icon-right">
+          {props.rightIcon}
+        </span>
+      </a>
+    )
+  }
+  return (
+    <div className="dropdown">
+      <DropdownItem>My profile</DropdownItem>
+      <DropdownItem 
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}>
+
+
+      </DropdownItem>
+    </div>
+  )
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false)
+  return (
     <li className="nav-item">
-      <a href="" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+      {open && props.children}
     </li>
   )
 }
@@ -30,9 +63,12 @@ function NavItem(props){
 function App() {
   return (
     <Navbar>
-      <NavItem icon={<PlusIcon/>}/>
-      <NavItem icon={<BellIcon/>}/>
-      <NavItem icon={<MessengerIcon/>}/>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
     </Navbar>
   );
 }
