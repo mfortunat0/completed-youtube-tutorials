9e1a84e6357a8772065d176c9cffbc69094edd0f
matheus <matheus.xmaz10@gmail.com>
2021-06-20 22:31:38 -0300

    next level shot

--- advanced-dropdown-menu-fireship/src/App.js
+++ advanced-dropdown-menu-fireship/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { useState } from 'react'
+import { useState,useRef, useEffect } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { ReactComponent as BellIcon } from './icons/bell.svg'
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg'
 import { ReactComponent as CaretIcon } from './icons/caret.svg'
@@ -21,10 +22,22 @@ function Navbar(props) {
 }
 
 function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main')
+  const [menuHeight, setMenuHeight] = useState(null)
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
 
   function DropdownItem(props) {
     return (
-      <a href="#" className="menu-item">
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         <span className="icon-button">
           {props.leftIcon}
         </span>
@@ -36,14 +49,50 @@ function DropdownMenu() {
     )
   }
   return (
-    <div className="dropdown">
-      <DropdownItem>My profile</DropdownItem>
-      <DropdownItem 
-        leftIcon={<CogIcon />}
-        rightIcon={<ChevronIcon />}>
-
-
-      </DropdownItem>
+    <div className="dropdown" ref={dropdownRef} style={{height: (menuHeight + 30)}}>
+      <CSSTransition in={activeMenu === "main"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>My profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+          </DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition in={activeMenu === "settings"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem
+            leftIcon={<ArrowIcon />}
+            goToMenu="main"
+          >
+          </DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   )
 }
