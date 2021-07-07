2de5eba01c343a738da5e59a4d7e165187bfe873
matheus <matheus.xmaz10@gmail.com>
2021-07-06 21:16:13 -0300

    completed criando-crud-com-nestjs-rocketseat

---
+++ criando-crud-com-nestjs-rocketseat/src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = new this.UserModel(createUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.UserModel.find();
+  }
+
+  findOne(id: string) {
+    return this.UserModel.findById(id);
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.UserModel.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        $set: updateUserDto,
+      },
+      {
+        new: true,
+      },
+    );
+  }
+
+  remove(id: string) {
+    return this.UserModel.deleteOne({
+      _id: id,
+    }).exec();
+  }
+}
